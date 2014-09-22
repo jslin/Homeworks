8ed6c2a3fbc7f53abea5c206da8aa48729ce4dd6
J. S. Lin <chunhsien.lin@gmail.com>
2014-09-23 00:13:30 +0800

    Fix the dList slice width bug.
    修正 dList 切片片段長度的錯誤。

--- CV/hw1a/src/ImageProcessing/DiagonalImage.py
+++ CV/hw1a/src/ImageProcessing/DiagonalImage.py
@@ -22,7 +22,7 @@ try:
 #
 # This loop dealing with image data into 2 dimension list pixel by pixel.
     for i in range(height):
-        dList.insert(i, imList[i:(i+1)*width])
+        dList.insert(i, imList[i*width:(i+1)*width])
    
 #   Diagonally mirrored the image.
     for i in range(1, len(dList)):
@@ -36,7 +36,7 @@ try:
 #    im.save("DLena.bmp")
     im.close()
 # For test Python's list elements swap function.
-#    a = [[1,2,3],[4,5,6],[7,8,9]]
+#    a = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 #    for i in range(1,len(a)):
 #        for j in range(i):
 #            a[i][j], a[j][i] = a[j][i], a[i][j]
