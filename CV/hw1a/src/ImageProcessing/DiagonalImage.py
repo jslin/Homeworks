a7ddc73d5ed77fc6e08e2242d0ea107461cbb436
J. S. Lin <chunhsien.lin@gmail.com>
2014-09-24 00:49:31 +0800

    Diagonal mirrored image
    You should new an image buffer, then put the diagonal mirrored image
    data, using putpixel() to do so, into the buffer. Then, put the list
    data to image buffer for show image.

--- CV/hw1a/src/ImageProcessing/DiagonalImage.py
+++ CV/hw1a/src/ImageProcessing/DiagonalImage.py
@@ -21,20 +21,23 @@ try:
     dList = []
 #
 # This loop dealing with image data into 2 dimension list pixel by pixel.
-    for i in range(height):
+    for i in (range(height)):
         dList.insert(i, imList[i*width:(i+1)*width])
    
+    newIm = Image.new("L",(512,512), 128)
 #   Diagonally mirrored the image.
     for i in range(1, len(dList)):
         for j in range(i):
-#            print("Before swap, i=", i,"j=",j, dList[i][j], dList[j][i])
             dList[i][j], dList[j][i] = dList[j][i], dList[i][j]
-#            print("After swap, i=", i,"j=",j, dList[i][j], dList[j][i])
-# Restore the up-side-down image data to image buffer.
-    im.putdata(dList)
-    im.show()
-#    im.save("DLena.bmp")
+            newIm.putpixel((i,j),dList[j][i])
+            newIm.putpixel((j,i),dList[i][j])
+            
+# Restore the diagonal mirrored image data to image buffer.
+    newIm.putdata(dList)
+    newIm.show()
+    newIm.save("DLena.bmp")
     im.close()
+    newIm.close()
 # For test Python's list elements swap function.
 #    a = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 #    for i in range(1,len(a)):
