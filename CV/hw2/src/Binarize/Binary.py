25cdf924516287be98389a214bb3dc14ee70beb6
J. S. Lin <chunhsien.lin@gmail.com>
2014-09-26 00:12:06 +0800

    Binarize lena image.
    CV homework 2. Binarize the lena image.

---
+++ CV/hw2/src/Binarize/Binary.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding:utf-8
+#
+# Copyright (c) 2014 Chun-Hsien Lin (D03922030). All right reserved.
+"""
+電腦視覺作業HW2 #1
+2014.09.30 v.0.1
+讀入 lena.bmp 圖檔後，將該圖檔二元化後輸出。Threshold = 128。
+使用 Pillow 程式庫來處理圖檔的讀寫。
+(http://pillow.readthedocs.org/en/latest/index.html)
+"""
+from __future__ import print_function
+from PIL import Image
+
+try:
+    im = Image.open("lena.bmp", "r")
+    im.load() # load image data into memory.
+    width = im.size[0]
+    height = im.size[1]
+    imList = list(im.getdata()) # Get the image data, then convert each pixel to an ordinary sequence.
+except IOError:
+    print("cannot open lena")
