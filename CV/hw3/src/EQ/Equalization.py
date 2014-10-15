faf9d378156bc96e4fdf9b4a6845ad81e1637eed
J. S. Lin <chunhsien.lin@gmail.com>
2014-10-15 12:02:25 +0800

    Fix calculating eqized image histogram  error.

--- CV/hw3/src/EQ/Equalization.py
+++ CV/hw3/src/EQ/Equalization.py
@@ -7,6 +7,9 @@ Created on 2014/10/07
 
 @author: Chun-Hsien Lin (D03922030)
 電腦視覺作業HW3 #1
+2014.10.15 v.0.2
+修正計算等化處理後 histogram 錯誤的問題。
+
 2014.10.07 v.0.1
 讀入 lena.im 圖檔後，作灰階等化處理。
 使用 Pillow 程式庫來處理 BMP 圖檔的讀寫。
@@ -49,11 +52,11 @@ logFile.write(str(histogram))
 logFile.close()
 newIm.show()
 
-logFile = open("histogram2.txt", "w")
+logFile = open("eqized.txt", "w")
 total_number_pixels = width * height
 accu_sum = 0
 s = []
-# Equalize the histogram.
+# Equalize the histogram and set new intensity value to each pixel.
 for i in range(len(histogram)):
     accu_sum = accu_sum + histogram[i]
     s.insert(i, 255 * accu_sum / total_number_pixels)
@@ -63,6 +66,16 @@ logFile.close()
 for i in range(height):
     for j in range(width):
         eqIm.putpixel((i,j), s[newIm.getpixel((i,j))])
+#
+# Calculating the equalized Lena image histogram
+histogram =[]
+eqImList = list(eqIm.getdata())
+for i in range(256):
+    histogram.append(eqImList.count(i))
+# Write out a histogram file.
+logFile = open("histogram2.txt", "w")
+logFile.write(str(histogram))
+logFile.close()
 eqIm.show()
 eqIm.save("lena_eq.bmp")
 eqIm.close()
