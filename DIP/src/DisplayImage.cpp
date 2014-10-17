50db2de631ce9c7216fd5b9cc47211d067818684
J. S. Lin <chunhsien.lin@gmail.com>
2014-10-17 18:59:30 +0800

    Create a project file for Xcode.
    Using the same source code for VS 2013 and Xcode.

--- DIP/src/DisplayImage.cpp
+++ DIP/src/DisplayImage.cpp
@@ -1,22 +1,66 @@
-#include <opencv2/opencv.hpp>
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by 木村盆栽 on 2014/10/3.
+//  Copyright (c) 2014年 Chun-Hsien Lin. All rights reserved.
+//
+#include <iostream>
+#include <fstream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv/cvaux.hpp"
 
+using namespace std;
 using namespace cv;
 
-int main( int argc, char** argv )
+
+int main(int argc, const char * argv[])
 {
-  Mat image;
-  image = imread( argv[1], 1 );
 
-  if( argc != 2 || !image.data )
+    IplImage * image;
+    int height, width, step, channels;
+    uchar *data;
+    int i, j, k;
+
+
+    image = cvLoadImage( argv[1], 1);
+
+
+    if ( argc != 2 || !image)
     {
-      printf( "No image data \n" );
-      return -1;
+        printf( "Could not load image file: %s\n" , argv [1]);
+        exit(0);
     }
+    // get the image data
+    height = image->height;
+    width = image->width;
+    step = image->widthStep;
+    channels = image->nChannels;
+    data = ( uchar *)image->imageData;
+    printf( "Processing a %dx%d image with %d channels\n" , height, width, channels);
 
-  namedWindow( "Display Image", WINDOW_AUTOSIZE );
-  imshow( "Display Image", image );
+    // create a window
+    cvNamedWindow( "mainWin", CV_WINDOW_AUTOSIZE );
+    cvMoveWindow( "mainWin", 100, 100);
 
-  waitKey(0);
 
-  return 0;
+    // invert the image
+    for (i = 0; i<height; i++)
+        for (j = 0; j<width; j++)
+            for (k = 0; k<channels; k++)
+                data[i*step + j*channels + k] = 255 - data[i*step + j*channels + k];
+
+    // show the image
+    cvShowImage( "mainWin", image);
+
+
+    // wait for a key
+    cvWaitKey(0);
+
+
+    // release the image
+    cvReleaseImage(&image);
+    return 0;
 }
+
