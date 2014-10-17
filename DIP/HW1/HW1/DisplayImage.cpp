49f028d46331226ae113398db436f476c395819d
J. S. Lin <chunhsien.lin@gmail.com>
2014-10-17 11:13:03 +0800

    Refine DIP homework 1 for cross-platform compatibility.
    Refine DIP homework 1 for cross-platform compatibility. Old code are
    commentized.

--- DIP/HW1/HW1/DisplayImage.cpp
+++ DIP/HW1/HW1/DisplayImage.cpp
@@ -1,22 +1,55 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main(int argc, char** argv)
 {
-	Mat image;
-	image = imread(argv[1], 1);
+//	Mat image;
+//	image = imread(argv[1], 1);
+	IplImage * image;
+	int height, width, step, channels;
+	uchar *data;
+	int i, j, k;
 
-	if (argc != 2 || !image.data)
+	image = cvLoadImage(argv[1], 1);
+
+	if (argc != 2 || !image)
 	{
-		printf("No image data \n");
-		return -1;
+		printf("Could not load image file: %s\n", argv[1]);
+		exit(0);
 	}
+	// get the image data
+	height = image->height;
+	width = image->width;
+	step = image->widthStep;
+	channels = image->nChannels;
+	data = (uchar *)image->imageData;
+	printf("Processing a %dx%d image with %d channels\n", height, width, channels);
 
-	namedWindow("Display Image", WINDOW_AUTOSIZE);
-	imshow("Display Image", image);
+	// create a window
+	cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
+	cvMoveWindow("mainWin", 100, 100);
 
-	waitKey(0);
+	// invert the image
+	for (i = 0; i<height; i++) for (j = 0; j<width; j++) for (k = 0; k<channels; k++)
+		data[i*step + j*channels + k] = 255 - data[i*step + j*channels + k];
 
+	// show the image
+	cvShowImage("mainWin", image);
+
+	// wait for a key
+	cvWaitKey(0);
+
+	// release the image
+	cvReleaseImage(&image);
 	return 0;
 }
+//	namedWindow("Display Image", WINDOW_AUTOSIZE);
+//	imshow("Display Image", image);
+
+//	waitKey(0);
+
+//	return 0;
+//}
