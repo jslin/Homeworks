398135ceb55851d4c3de989d4682c45bbbdaef01
J. S. Lin <chunhsien.lin@gmail.com>
2014-11-14 23:34:57 +0800

    CV Homework #5
    Gray scale morphology completed.

--- CV/CV HW5/src/Morphology/grayDilation.py
+++ CV/CV HW5/src/Morphology/grayDilation.py
@@ -6,14 +6,15 @@
 電腦視覺作業HW5 #1
 2014.11.14 v.0.1
 讀入 lena.bmp 圖檔後，將該圖檔做 mathematical morphology (gray scale Erosion)。
-本模組處理灰階圖的 erosion 運算。
+本模組處理灰階圖的 dilation 運算。
 使用 Pillow 程式庫來處理圖檔的讀寫。
 (http://pillow.readthedocs.org/en/latest/index.html)
 """
 from __future__ import print_function
 from PIL import Image
 
-def grayErosion(imageBuffer, kernel):
+
+def grayDilation(imageBuffer, kernel):
     try:
         imageBuffer.load() # load image data into memory.
         width = imageBuffer.size[0]
@@ -21,28 +22,27 @@ def grayErosion(imageBuffer, kernel):
         inputIm = imageBuffer.copy()
 
     # Create a blank image, fill up with color = 0 (white).
-        eroIm = Image.new("L",(width, height), 0)
-    # Using Erosion definition to implement.
+        dilIm = Image.new("L",(width, height), 0)
+    # Using Dilation definition to implement.
     #
         for i in range(width):
             for j in range(height):
-                minValue = 255
-                # for kernel b, check x + b are belong to a or not.
+                maxValue = 0
                 for k in range(len(kernel)):
                     p = kernel[k][0]
                     q = kernel[k][1]
                     value = kernel[k][2]
                     temp = inputIm.getpixel((i,j))
-                    x = i + p
-                    y = j + q
+                    x = i - p
+                    y = j - q
                     if (x >= 0) and (x < width) and (y >= 0) and (y < height):
-                        temp = inputIm.getpixel((x,y)) - value
-                    if temp < minValue:
-                        minValue = temp
-                    eroIm.putpixel((i,j), minValue)
+                        temp = inputIm.getpixel((x,y)) + value
+                    if temp > maxValue:
+                        maxValue = temp
+                    dilIm.putpixel((i,j), maxValue)
         inputIm.close()
     except IOError:
         print("cannot open lena")
     finally:
         inputIm.close()
-    return eroIm
+    return dilIm
--- CV/CV HW5/src/Morphology/main.py
+++ CV/CV HW5/src/Morphology/main.py
@@ -12,22 +12,38 @@
 """
 from __future__ import print_function
 # from Morphology import *
-# import grayDilation
+import grayDilation
 import grayErosion
 from PIL import Image
 
 def main():
     # Define some kernels.
     #
-    octangon = [(-1,2,10),(0,2,10),(1,2,10),(-2,1,10),(-1,1,10),(0,1,10),(1,1,10),(2,1,10),(-2,1,10),(-1,1,10),(0,1,10),(1,1,10),(2,1,10),(-2,0,10),(-1,0,10),(0,0,10),(1,0,10),(2,0,10),(-2,-1,10),(-1,-1,10),(0,-1,10),(1,-1,10),(2,-1,10),(-1,-2,10),(0,-2,10),(1,-2,10)]
+#    octangon = [(-1,2,10),(0,2,10),(1,2,10),(-2,1,10),(-1,1,10),(0,1,10),(1,1,10),(2,1,10),(-2,1,10),(-1,1,10),(0,1,10),(1,1,10),(2,1,10),(-2,0,10),(-1,0,10),(0,0,10),(1,0,10),(2,0,10),(-2,-1,10),(-1,-1,10),(0,-1,10),(1,-1,10),(2,-1,10),(-1,-2,10),(0,-2,10),(1,-2,10)]
+    octangon = [(-1,2,0),(0,2,0),(1,2,0),(-2,1,0),(-1,1,0),(0,1,0),(1,1,0),(2,1,0),(-2,1,0),(-1,1,0),(0,1,0),(1,1,0),(2,1,0),(-2,0,0),(-1,0,0),(0,0,0),(1,0,0),(2,0,0),(-2,-1,0),(-1,-1,0),(0,-1,0),(1,-1,0),(2,-1,0),(-1,-2,0),(0,-2,0),(1,-2,0)]
     im = Image.open("lena.bmp", "r")
 
     # Erosion
     eroImage = grayErosion.grayErosion(im, octangon)
     eroImage.show()
     eroImage.save("Erosion_lena.bmp")
-
+    # Dilation
+    dilImage = grayDilation.grayDilation(im, octangon)
+    dilImage.show()
+    dilImage.save("Dilation_lena.bmp")
+    # Opening
+    openingImage = grayDilation.grayDilation(eroImage, octangon)
+    openingImage.show()
+    openingImage.save("Opening_lena.bmp")
+    # Closing
+    closingImage = grayErosion.grayErosion(dilImage, octangon)
+    closingImage.show()
+    closingImage.save("Closing_lena.bmp")
+    
     eroImage.close()
+    dilImage.close()
+    openingImage.close()
+    closingImage.close()
 # -- end of main()
     
 main()
