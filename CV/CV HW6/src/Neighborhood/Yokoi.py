4d7cc06df044ee2a45d5efe5b801e28e6d364bad
J. S. Lin <chunhsien.lin@gmail.com>
2014-11-19 01:19:48 +0800

    CV Homework #6

---
+++ CV/CV HW6/src/Neighborhood/Yokoi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# coding:utf-8
+#
+# Copyright (c) 2014 Chun-Hsien Lin (D03922030). All right reserved.
+"""
+電腦視覺作業HW6 #1
+2014.11.19 v.0.1
+讀入 lena.bmp 圖檔後，將該圖檔做 resampling 至 64x64。
+再做Yokoi。
+使用 Pillow 程式庫來處理圖檔的讀寫。
+(http://pillow.readthedocs.org/en/latest/index.html)
+"""
